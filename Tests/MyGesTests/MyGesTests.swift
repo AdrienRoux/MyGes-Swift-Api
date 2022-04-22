b5589308884a08aac2c33c0f295da9e8051a2552
Ad <adrienroux94@gmail.com>
2022-04-22 15:09:14 +0200

    test

--- Tests/MyGesTests/MyGesTests.swift
+++ Tests/MyGesTests/MyGesTests.swift
@@ -6,6 +6,6 @@ final class MyGesTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MyGes().text, "Hello, World!")
+        XCTAssertEqual(MyGes().text, "Hello, World! AHAH")
     }
 }
