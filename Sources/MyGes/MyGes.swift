f42208bbd2ee863d0037d9f4db90d6af71be3af8
Ad <adrienroux94@gmail.com>
2022-07-06 16:10:35 +0200

    credentials on failure #patch

--- Sources/MyGes/MyGes.swift
+++ Sources/MyGes/MyGes.swift
@@ -13,7 +13,6 @@ public struct MyGes {
                 case .success:
                     completion(nil)
                 case .failure(let error):
-					MyGes.credentials = nil
                     completion(error)
                 }
             }
